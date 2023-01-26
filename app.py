ba498540fd314c96f6f54924a1bc03b47141f5f1
David-Mateus <david-mateus09@hotmail.com>
2023-01-26 12:15:08 -0300

    versao 1.2

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
 import gtts
 from playsound import playsound
-import os
+import time
 
-with open('texto.txt', 'r') as arquivo:
-    for linha in arquivo:
-        frase = gtts.gTTS(linha, lang='pt-br')
-        frase.save('frase.mp3')
-        playsound('frase.mp3')
-        
+def createFile(name):
+    arquivo = open(name+'.txt', 'w+')
+    print("Arquivo criado")
+
+qtsFile = int(input('Quantos arquivos você deseja criar? '))
+i = 0
+listOfFile = []
+while i < qtsFile:
+    nameFile = str(input('Nome dos arquivos: '))
+    listOfFile.append(nameFile)
+    i += 1
+for arquivo in listOfFile:
+    createFile(arquivo)
+   
+
+def addText(text, file):
+    with open(file+".txt", "w", encoding="utf-8") as arquivo:
+        arquivo.write(text)
+
+for file in range(len(listOfFile)):
+    choiceFile = str(input("Digite o nome do arquivo: "))
+    for i in listOfFile:
+        if choiceFile == i:
+            textFile = str(input("Digite o texto: "))
+            addText(textFile, choiceFile)
+    
+# def speakPython():
+for arquivo in listOfFile:
+    nameFileOpen = arquivo
+    with open(f"{nameFileOpen}.txt", 'r') as arquivo:
+        for linha in arquivo:
+            frase = gtts.gTTS(linha, lang='pt-br')
+            frase.save(f"{nameFileOpen}.mp3")
+            # playsound(f"{nameFileOpen}.mp3")
+
+
+
+
